49fe141146661d5cf9e587bb2e95f5f644015e95
TJB543 <58111256+TJB543@users.noreply.github.com>
2019-11-24 10:58:04 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -343,6 +343,7 @@ function draw() {
     background(255);
     stroke(255);
     image(umbrella,width/2-35,height/4,100,100)
+    image(umbrella,width/2,height/4,10,10);
     for (let i = 0; i < amount; i++) {
       if (enemy[i].side < 2) {
         enemy[i].update();
